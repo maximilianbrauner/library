3f5ad7364235b340cde52681403175b04b1924ce
maximusheaven <maximilianbrauner95@gmail.com>
2024-11-06 12:26:27 +0100

    functionality for adding books via a button and some styling

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const bookContainer = document.querySelector(".book-container");
+const dialog = document.querySelector("dialog")
+const addBookBtn = document.querySelector("#showModal")
+const confirmNewBookBtn = dialog.querySelector("#confirmNewBook")
+const bookListContainer = document.querySelector(".book-list-container");
+
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -11,32 +15,44 @@ Book.prototype.info = function () {
   return `${this.title} by ${this.author} has ${this.pages} Pages, ${this.read}`;
 };
 
-const theHobbit = new Book(
-  "The Hobbit",
-  "J.R.R Tolkien",
-  "295",
-  "not read yet"
-);
-  const hpOtter = new Book("Harry Potter", "JK Rowling", "469", "always");
-
 bookList = [];
-function addBookToLibrary(book) {
+function addBookToLibrary(title, author, pages, read) {
+  book = new Book(title, author, pages, read)
   bookList.push(book);
-  console.log(bookList);
+  console.log(bookList)
+  displayBooks()
 }
-  
+
 function displayBooks() {
+  bookListContainer.innerHTML = ''
   bookList.forEach((elem) => {
-    const displayBook = document.createElement("div");;
+    const displayBook = document.createElement("div");
     displayBook.innerHTML += `
     <h3>Title: ${elem.title}</h3>
     <p>Author: ${elem.author}</p>
     <p>Pages: ${elem.pages}</p>
-    <p>Read: ${elem.read}</p>
+    <p>Read: ${elem.read ? "Read" : "Not Read"}</p>
     `;
-    bookContainer.appendChild(displayBook)
+    bookListContainer.appendChild(displayBook);
     displayBook.classList.add("card");
   });
 }
-addBookToLibrary(theHobbit)
-addBookToLibrary(hpOtter)
+// addBookToLibrary(theHobbit);
+// addBookToLibrary(hpOtter);
+
+addBookBtn.addEventListener('click', () => {
+  dialog.showModal()
+})
+
+
+confirmNewBookBtn.addEventListener('click', (event) => {
+  event.preventDefault()
+  const bookTitle = title.value
+  const bookAuthor = author.value
+  const bookPages = pages.value
+  const bookReadStatus = read.checked
+  console.log(`${title}`,`${author}`,`${pages}`,`${read}`)
+  addBookToLibrary(bookTitle, bookAuthor, parseInt(bookPages), bookReadStatus)
+  dialog.close()
+})
+
